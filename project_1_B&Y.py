93b1a64015fbe08ef441148e93d184cad3c2c4af
Benson Chan <benson@BenBens-MacBook-Pro-4.local>
2025-10-09 02:24:10 -0400

    Finished the load_data method to read from csv files, then test reading the file penguins.csv.

--- project_1_B&Y.py
+++ project_1_B&Y.py
@@ -4,4 +4,25 @@
 # Who did I work with: Zhiyu (Yuki) Pu
 # How I used generative AI:
 
+import unittest
+import os
 import csv
+
+def load_data(f):
+
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    full_path = os.path.join(base_path, f)
+
+    rows = []
+
+    with open(f, 'r') as file:
+        r = csv.DictReader(file)
+        for row in r:
+            rows.append(row)
+
+
+    return rows
+
+if __name__ == '__main__':
+    data = load_data('penguins.csv')
+    print(data)
