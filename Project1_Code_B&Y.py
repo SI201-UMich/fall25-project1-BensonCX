ebc8ab5d685af4ab1f5e187ec2c5dc3b39e2c708
Benson Chan <benson@BenBens-MacBook-Pro-4.local>
2025-10-14 19:49:31 -0400

    Benson: Completed the load_data method to read the penguins.csv file and tried to print the output to make sure the data is loaded correctly.

--- Project1_Code_B&Y.py
+++ Project1_Code_B&Y.py
@@ -2,3 +2,27 @@
 # Student ID: Chen: 3617 1540, Pu: 2783 7481
 # Email: chexfeii@umich.edu, zhiyupu@umich.edu
 # How we used generative AI:
+
+import os
+import csv
+
+#Read penguins.csv file
+def load_data (f):
+
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    full_path = os.path.join(base_path, f)
+
+    rows = []
+
+    with open(f, 'r') as file:
+        r = csv.DictReader(file)
+
+        for row in r:
+            rows.append(row)
+
+    return rows
+
+if __name__ == "__main__":
+    data = load_data('penguins.csv')
+    
+    print(data)
