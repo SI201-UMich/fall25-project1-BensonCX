bcd1d397e3551b268adae9efd368edbb33036683
Benson Chan <benson@BenBens-MacBook-Pro-4.local>
2025-10-14 20:50:30 -0400

    Yuki: Created the clean_and_cast method to dectect the lost value like 'NA' and changed to None in order to make the calculation more convenient.

--- Project1_Code_B&Y.py
+++ Project1_Code_B&Y.py
@@ -7,22 +7,50 @@ import os
 import csv
 
 #Read penguins.csv file
-def load_data (f):
+def load_data(f):
 
     base_path = os.path.abspath(os.path.dirname(__file__))
     full_path = os.path.join(base_path, f)
 
-    rows = []
+    data = []
 
     with open(f, 'r') as file:
         r = csv.DictReader(file)
 
         for row in r:
-            rows.append(row)
+            data.append(row)
 
-    return rows
+    return data
 
-if __name__ == "__main__":
-    data = load_data('penguins.csv')
+#Drops rows with invalid or missing data
+def clean_and_cast(data):
+
+    cleaned_data = []
+
+    for row in data:
+        
+        new_row = {}
+
+        for key, value in row.items():
+            cleaned_value = str(value).strip()
+
+            if cleaned_value == '' or cleaned_value.lower() == 'na':
+                new_row[key] = None
+
+            else:
+                try:
+                    new_row[key] = float(cleaned_value)
+
+                except ValueError:
+                    new_row[key] = cleaned_value
+
+        cleaned_data.append(new_row)
+
+    return cleaned_data
+
+if __name__ == '__main__':
     
-    print(data)
+    data = load_data('penguins.csv')
+    cleaned_data = clean_and_cast(data)
+
+    print(cleaned_data)
